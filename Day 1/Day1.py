cc352022c8633d0537688d4b982d18ffb0296be0
Satyam Rana <57563008+satyamranatc@users.noreply.github.com>
2024-02-19 11:57:08 +0530

    first commit

---
+++ Day 1/Day1.py
@@ -0,0 +1,32 @@
+# x = 5
+# y = 33
+# z = x + y
+# print(z)
+
+# print("The Result Of 9 + 7 = ",9+7)
+
+# Basic Data Types
+# x = "String"
+# x = 4             # Integer
+# x = 4.4           # Float
+# x = True          # Boolean
+
+
+# User Input:
+
+
+# name = input("Enter Your Name: ")
+# print("Hi", name)
+
+# a = int(input("Enter Num1:-"))
+# b = int(input("Enter Num2:-"))
+
+# print(a+b)
+
+
+# print("Satyam ",end="-")
+# print("Rana")
+# print("Satyam \n" * 5)
+
+
+print("Hello",input("Enter a Name: "))
---
+++ Day 2/Day2.py
@@ -0,0 +1,43 @@
+# Flow Control:
+# 1. If Else
+# 2. Loops
+
+
+#  The if statement:
+
+# age = 14
+
+# if age >100 or age < 1:
+#     print("Invalid age")
+
+# elif age >= 18:
+#     print("You Can Drive")
+#     if age >= 70 and age <=100:
+#         print("Drive Carefully")
+
+
+# elif age >= 10:
+#     print("You Can Ride a Cycle")
+
+# else:
+#     print("You Can't Drive or Ride")
+
+
+
+# Match case:
+Day = 4
+
+
+# match Day:
+
+#     case 1:
+#         print("Monday")
+
+#     case 2:
+#         print("Tuesday")
+
+#     case 3:
+#         print("Wednesday")
+
+#     case _:
+#         print("Not a valid day")
