0197c1f4b61330cd29f27ff84d79ba3a2764ac42
Satyam Rana <57563008+satyamranatc@users.noreply.github.com>
2024-02-21 12:06:09 +0530

    Day - 4

--- Day 3/Day3.py
+++ Day 3/Day3.py
@@ -36,20 +36,59 @@
 
 #Check IF A Person id eligible for dirving or not.
     
-age = int(input("Enter your age: "))
+# age = int(input("Enter your age: "))
 
-if age>100 or age < 1:
-    print("Invalid age")
+# if age>100 or age < 1:
+#     print("Invalid age")
 
-elif age >= 18:
-    print("You can drive")
-    if age>70:
-        print("Drive Care Fully")
+# elif age >= 18:
+#     print("You can drive")
+#     if age>70:
+#         print("Drive Care Fully")
         
-elif age >= 10:
-    print("You can ride a cycle")
+# elif age >= 10:
+#     print("You can ride a cycle")
 
-else:
-    print("You can't drive")
+# else:
+#     print("You can't drive")
+
+
+
+# Check a Trinange is Valid and Right Angled Or Not...
+
+# Angle1 = int(input("Enter the Angle1:- "))
+# Angle2 = int(input("Enter the Angle2:- "))
+# Angle3 = int(input("Enter the Angle3:- "))
+
+
+# if (Angle1 + Angle2 + Angle3) != 180:
+#     print("Not a Triangle")
+
+# else:
+#     if (Angle1 == 90 or Angle2 == 90 or Angle3 == 90):
+#         print("Its is a Right angle triangle")
+
+#     else:
+#         print("Its is a Triangle") 
+
+
+# -----------------Same Solution----------------
+        
+# Angle1 = int(input("Enter the Angle1:- "))
+# Angle2 = int(input("Enter the Angle2:- "))
+# Angle3 = int(input("Enter the Angle3:- "))
+
+
+# if (Angle1 + Angle2 + Angle3) == 180:
+#     if (Angle1 == 90 or Angle2 == 90 or Angle3 == 90):
+#         print("Its is a Right angle triangle")
+
+#     else:
+#         print("Its is a Triangle") 
+
+# else:
+#     print("Not a Triangle")
+
+
 
 
