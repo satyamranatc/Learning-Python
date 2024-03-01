35cb392e976c0b6478d64fca0ada67af07ee7911
Satyam Rana <57563008+satyamranatc@users.noreply.github.com>
2024-03-01 11:47:33 +0530

    Functions

---
+++ Day 11/Day11.py
@@ -0,0 +1,35 @@
+#Dictonery
+
+p = {
+    "name": "Naman",
+    "age": 25,
+    "city": "Bangalore"
+}
+
+# p.pop("name")
+# p.popitem()
+# p.update({"name": "Nitin"})
+# p.clear()
+# p.keys()
+# p.values()
+# p.items()
+
+
+#Set:
+s =  {1,2,3,4,5,6,7}
+s2 = {6,7,8,9,10}
+# print(s.union(s2))
+# print(s.intersection(s2))
+# print(s.difference(s2))
+# print(s2.difference(s))
+# print(s.symmetric_difference(s2))
+
+
+
+
+
+
+
+
+
+
---
+++ Day 11/Day11_2.py
@@ -0,0 +1,49 @@
+# def Greet():
+#     print("Hi")
+#     print("Good Morning")
+#     print("How are you?")
+#     print("----------------")
+
+
+# print("Ajay")
+# Greet()
+
+# print("Naman")
+# Greet()
+
+
+# print("Hitesh")
+# Greet()
+
+
+#Arguments
+# def Add(a,b):
+#     print(a+b)
+
+
+# Add(3,5)
+# Add(4,6)
+# Add(89,7)
+# Add(2,4)
+
+
+#1 More Example:
+
+def Check(a):
+    if a % 2 == 0:
+        print(a, "is even")
+    else:
+        print(a, "is odd")
+
+
+
+Check(4)
+Check(41)
+Check(1)
+Check(9)
+
+#also With List
+
+# l = [1,2,3,4,5]
+# for i in l:
+#     Check(i)
